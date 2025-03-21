3294b5ed25c1b1bdfd30f17bc457148d4740ec40
Marco AFM <160976706+marco-afm@users.noreply.github.com>
2025-03-21 14:57:30 -0300

    Até o áudio funcionar certo pt03

--- assets/js/iniciaAudio.js
+++ assets/js/iniciaAudio.js
@@ -1,18 +1,10 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const audio = document.getElementById("audioFundo");
-
-//     document.body.addEventListener("click", function() {
-//         if (audio.paused) {
-//             audio.play();
-//         }
-//     });
-// });
-
-const btnAbrir = document.querySelector(".btnAbrir");
-const audioFundo = document.getElementById("audioFundo");
-
-audioFundo.paused();
-
-btnAbrir.addEventListener("click", function() {
-    audioFundo.play();
+document.addEventListener("DOMContentLoaded", function() {
+    const audioFundo = document.getElementById("audioFundo");
+    audioFundo.paused();
+    
+    audioFundo.addEventListener("click", function() {
+        if (audio.paused) {
+            audio.play();
+        }
+    });
 });
