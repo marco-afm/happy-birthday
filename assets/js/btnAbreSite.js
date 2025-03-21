2359c195512672319b07afbc04617a1f08f1d8b7
Marco AFM <160976706+marco-afm@users.noreply.github.com>
2025-03-21 15:14:43 -0300

    Até o áudio funcionar certo pt07

--- assets/js/btnAbreSite.js
+++ assets/js/btnAbreSite.js
@@ -1,6 +1,11 @@
+// ========== Botão abre presente ========== //
 const btnAbrir = document.querySelector(".btnAbrir");
 const modalAbrir = document.querySelector(".modalAbrir");
+const audioFundo = document.getElementById("audioFundo");
 
 btnAbrir.addEventListener("click", function() {
     modalAbrir.style.display = "none";
+    if (audioFundo.paused) {
+        audioFundo.play();
+    }
 });
